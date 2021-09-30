e4f0f58f30feedb0afed4f94d73a11305eb81e66
almostSouji <timoqueezle@gmail.com>
2021-09-30 09:33:07 +0200

    chore: redo emojis again

--- src/index.ts
+++ src/index.ts
@@ -207,17 +207,17 @@ export function start() {
 								const isContentMatch = tag.content.toLowerCase().includes(args.query);
 								if (exactKeyword) {
 									exactKeywords.push({
-										name: `${key}`, // ⭐
+										name: `⭐ ${key}`,
 										value: key,
 									});
 								} else if (includesKeyword) {
 									keywordMatches.push({
-										name: `${key}`, // 🏷️
+										name: `🏷️ ${key}`,
 										value: key,
 									});
 								} else if (isContentMatch) {
 									contentMatches.push({
-										name: `${key}`, // 📄
+										name: `📄 ${key}`,
 										value: key,
 									});
 								}
@@ -229,7 +229,7 @@ export function start() {
 									.filter((t) => t.hoisted)
 									.map((_, key) => {
 										return {
-											name: `${key}`, // 📌
+											name: `📌 ${key}`,
 											value: key,
 										};
 									}),
