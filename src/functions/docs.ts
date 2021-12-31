7adcbe205911bbde4045914546a76ea9316deeba
Jeroen Claassens <support@favware.tech>
2021-12-31 20:00:08 +0100

    feat(djs): rewrite to new library (#59)

--- src/functions/docs.ts
+++ src/functions/docs.ts
@@ -1,5 +1,5 @@
 import { bold, formatEmoji, hideLinkEmbed, hyperlink, underscore } from '@discordjs/builders';
-import Doc from 'discord.js-docs';
+import { Doc, DocElement, DocTypes, SourcesStringUnion } from 'discordjs-docs-parser';
 import { Response } from 'polka';
 import { suggestionString, truncate } from '../util';
 import { prepareErrorResponse, prepareResponse, prepareSelectMenu } from '../util/respond';
@@ -18,47 +18,56 @@ import {
 	EMOJI_ID_METHOD_DEV,
 } from '../util/constants';
 
-function docTypeEmojiId(docType: string, dev = false): string {
+function docTypeEmojiId(docType: DocTypes | null, dev = false): string {
 	switch (docType) {
-		case 'typedef':
+		case DocTypes.Typedef:
 			return dev ? EMOJI_ID_INTERFACE_DEV : EMOJI_ID_INTERFACE;
-		case 'prop':
+		case DocTypes.Prop:
 			return dev ? EMOJI_ID_FIELD_DEV : EMOJI_ID_FIELD;
-		case 'class':
+		case DocTypes.Class:
 			return dev ? EMOJI_ID_CLASS_DEV : EMOJI_ID_CLASS;
-		case 'method':
+		case DocTypes.Method:
 			return dev ? EMOJI_ID_METHOD_DEV : EMOJI_ID_METHOD;
-		case 'event':
+		case DocTypes.Event:
 			return dev ? EMOJI_ID_EVENT_DEV : EMOJI_ID_EVENT;
 		default:
 			return dev ? EMOJI_ID_DJS_DEV : EMOJI_ID_DJS;
 	}
 }
 
-function escapeMDLinks(s = ''): string {
-	return s.replace(/\[(.+?)\]\((.+?)\)/g, '[$1](<$2>)');
-}
-
 function stripMd(s = ''): string {
 	return s.replace(/[`\*_]/gi, '');
 }
 
-function formatInheritance(prefix: string, inherits: DocElement[], doc: Doc): string {
-	const res = inherits.map((element: any) => element.flat(5));
-	return ` (${prefix} ${res.map((element) => escapeMDLinks(doc.formatType(element))).join(' and ')})`;
+function extractGenericTypeInfill(type: string): string {
+	const match = /<(?<type>[A-Za-z]+)>/.exec(type);
+	return match?.groups?.type ? match.groups.type : type;
+}
+
+function formatInheritance(prefix: string, inherits: string[][], doc: Doc): string {
+	const res = inherits.flatMap((element) => {
+		if (Array.isArray(element)) return element.flat(5);
+		return [element];
+	});
+
+	const inheritedLinks = res.map((element) => doc.get(extractGenericTypeInfill(element))?.link).filter(Boolean);
+
+	if (!inheritedLinks.length) return '';
+
+	return ` (${prefix} ${inheritedLinks.join(' and ')})`;
 }
 
 export function resolveElementString(element: DocElement, doc: Doc): string {
 	const parts = [];
 	if (element.docType === 'event') parts.push(`${bold('(event)')} `);
 	if (element.static) parts.push(`${bold('(static)')} `);
-	parts.push(underscore(bold(escapeMDLinks(element.link ?? ''))));
+	parts.push(underscore(bold(element.link)));
 	if (element.extends) parts.push(formatInheritance('extends', element.extends, doc));
 	if (element.implements) parts.push(formatInheritance('implements', element.implements, doc));
 	if (element.access === 'private') parts.push(` ${bold('PRIVATE')}`);
 	if (element.deprecated) parts.push(` ${bold('DEPRECATED')}`);
 
-	const s = escapeMDLinks(element.formattedDescription ?? element.description ?? '').split('\n');
+	const s = ((element.formattedDescription || element.description) ?? '').split('\n');
 	const description = s.length > 1 ? `${s[0]} ${hyperlink('(more...)', hideLinkEmbed(element.url ?? ''))}` : s[0];
 
 	return `${parts.join('')}\n${description}`;
@@ -75,7 +84,13 @@ function buildSelectOption(result: DocElement, dev = false) {
 	};
 }
 
-export function fetchDocResult(source: string, doc: Doc, query: string, user?: string, target?: string): string | null {
+export function fetchDocResult(
+	source: SourcesStringUnion,
+	doc: Doc,
+	query: string,
+	user?: string,
+	target?: string,
+): string | null {
 	const element = doc.get(...query.split(/\.|#/));
 	if (!element) return null;
 	const icon = formatEmoji(docTypeEmojiId(element.docType, source === 'main'));
@@ -85,7 +100,7 @@ export function fetchDocResult(source: string, doc: Doc, query: string, user?: s
 export function djsDocs(
 	res: Response,
 	doc: Doc,
-	source: string,
+	source: SourcesStringUnion,
 	query: string,
 	user?: string,
 	target?: string,
