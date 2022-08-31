4120a6dbbf078d7de54ee0e8b336e89394795b01
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-08-31 10:02:08 +0100

    refactor(*): use MessageFlags enum (#116)

--- src/util/respond.ts
+++ src/util/respond.ts
@@ -1,4 +1,4 @@
-import { InteractionResponseType } from 'discord-api-types/v10';
+import { InteractionResponseType, MessageFlags } from 'discord-api-types/v10';
 import { Response } from 'polka';
 import { PREFIX_FAIL } from './constants';
 
@@ -19,7 +19,7 @@ export function prepareResponse(
 		JSON.stringify({
 			data: {
 				content,
-				flags: ephemeral ? 64 : 0,
+				flags: ephemeral ? MessageFlags.Ephemeral : 0,
 				// eslint-disable-next-line @typescript-eslint/naming-convention
 				allowed_mentions: { parse, users },
 				components: [],
