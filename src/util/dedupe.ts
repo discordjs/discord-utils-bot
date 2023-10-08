06caf434ca5cd2a9bc3a96fcb7498acb4de7b616
almostSouji <timoqueezle@gmail.com>
2023-10-08 18:28:30 +0200

    fix(algolia): hierarchy-based dedupe approach
    resolves #183

--- src/util/dedupe.ts
+++ src/util/dedupe.ts
@@ -2,5 +2,8 @@ import type { AlgoliaHit } from '../types/algolia.js';
 
 export function dedupeAlgoliaHits(): (hit: AlgoliaHit) => boolean {
 	const dedupe = new Set<string>();
-	return (hit: AlgoliaHit) => Boolean(!dedupe.has(hit.objectID) && dedupe.add(hit.objectID));
+	return (hit: AlgoliaHit) => {
+		const dedupeIdentifier = Object.values(hit.hierarchy).join('::');
+		return Boolean(!dedupe.has(dedupeIdentifier) && dedupe.add(dedupeIdentifier));
+	};
 }
